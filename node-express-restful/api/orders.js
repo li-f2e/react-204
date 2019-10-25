12b9bd09817cd9457aeece12fb0983047c3bae0f
Eddy Chang <eddy@joomla.com.tw>
2019-10-25 09:08:48 +0800

    format

--- node-express-restful/api/orders.js
+++ node-express-restful/api/orders.js
@@ -1,19 +1,19 @@
-import express from "express";
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message:"Orders listed."
-    });
-});
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders listed.',
+  })
+})
 
-router.get("/:orderId", (req, res, next) => {
-    var oid = req.params.orderId;
-    res.status(200).json({
-        message:`You are at spcific orders page for id ${oid}.`,
-        id: oid
-    });
-});
+router.get('/:orderId', (req, res, next) => {
+  var oid = req.params.orderId
+  res.status(200).json({
+    message: `You are at spcific orders page for id ${oid}.`,
+    id: oid,
+  })
+})
 
-module.exports = router;
+module.exports = router
--- node-express-restful/server.js
+++ node-express-restful/server.js
@@ -1,9 +1,9 @@
-import http from "http";
-import app from "./app";
+import http from 'http'
+import app from './app'
 
 //Use system configuration for port or use 6001 by default.
-const port = process.env.port || 6001;
+const port = process.env.port || 6001
 
 //Create server with exported express app
-const server = http.createServer(app);
-server.listen(port);
+const server = http.createServer(app)
+server.listen(port)
